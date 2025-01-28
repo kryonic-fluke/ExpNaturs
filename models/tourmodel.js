d0f91edac639889e7defd72f16753c901f02347b
Suyash Upadhyay <uranium235.a@gmail.com>
2025-01-28 13:34:20 +0530

    Change Mongoose Schema with more fields

--- models/tourmodel.js
+++ models/tourmodel.js
@@ -8,7 +8,9 @@ const tourSchema = new mongoose.Schema({
     name:{
       type:String,
       required:[true,'A tour must have a name'],
-      unique:true
+      unique:true,
+      trim:true
+
     },
     rating:{type:Number,
       default:4.5,
@@ -18,7 +20,64 @@ const tourSchema = new mongoose.Schema({
       type:Number,
       required:[true,'A tour must have a price']
     },
-  
+    duration:{
+        type:Number,
+        required:[true,"A tour must have durations"]
+    },
+
+    maxGroupSize:{
+        type:Number,
+        required:[true,"A tour must have maxGroupSize"]
+    },
+
+    difficulty:{
+        type:String,
+        required:[true,"A tour must have difficulty"]
+    },
+
+    ratingsAverage:{
+        type:Number,
+        rdefault:0
+    },
+
+    ratingsQuantity:{
+        type:Number,
+        default:0
+    },
+    
+    price:{
+        type:Number,
+        required:[true,"A tour must have difficulty"] 
+     },
+
+    priceDiscount:Number,
+
+
+    summary:{
+        type:String,
+        trim:true
+    },
+
+    decription:{
+        type:String,
+        trim:true
+    },
+
+    imageCover:{
+        type:String ,
+        required:[true,"A tour must have image Cover"] 
+    },
+    
+
+    images:[String],
+
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    },
+
+    startDates:[Date]
+
   }) 
   
   const Tour = mongoose.model('Tour',tourSchema);
